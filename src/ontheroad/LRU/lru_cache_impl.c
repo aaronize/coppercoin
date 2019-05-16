cd783142d3bd9cd38eac95bdcd098672cb46b220
aaronize <chendai2004@hotmail.com>
2019-05-16 23:45:03 +0800

    update

--- src/ontheroad/LRU/lru_cache_impl.c
+++ src/ontheroad/LRU/lru_cache_impl.c
@@ -7,6 +7,7 @@
 #include "lru_cache.h"
 #include "lru_cache_impl.h"
 
+// wrappers for posix semaphores
 void unix_error(char *msg)
 {
     fprintf(stderr, "%s: %s\n", msg, strerror(errno));
@@ -15,11 +16,11 @@ void unix_error(char *msg)
 
 void Sem_init(sem_t *sem, int pshared, unsigned int value)
 {
-    if (Sem_init(sem, pshared, value) < 0)
+    if (sem_init(sem, pshared, value) < 0)
         unix_error("Sem_init error");
 }
 
-vid P(sem_t *sem)
+void P(sem_t *sem)
 {
     if (sem_wait(sem) < 0)
         unix_error("P error");
@@ -28,11 +29,12 @@ vid P(sem_t *sem)
 void V(sem_t *sem)
 {
     if (sem_post(sem) < 0)
-        unix_error
+        unix_error;
 }
 
 static void freeList(LRUCacheS *cache);
 
+// LRU缓存及缓存单元接口实现
 // 创建一个缓存单元
 static cacheEntryS* newCacheEntry(char *key, char *data)
 {
@@ -70,4 +72,47 @@ int LRUCacheCreate(int capacity, void **lruCache)
     cache->cacheCapacity = capacity;
     Sem_init(&(cache->cache_lock), 0, 1);
     cache->hashMap = malloc(sizeof(cacheEntryS*)*capacity);
+    if (NULL == cache->hashMap)
+    {
+        free(cache);
+        perror("molloc");
+        return -1;
+    }
+
+    memset(cache->hashMap, 0, sizeof(cacheEntryS*)*capacity);
+    *lruCache = cache;
+    return 0;
+}
+
+// 释放一个LRU缓存
+int LRUCacheDestroy(void *lruCache)
+{
+    LRUCacheS* cache = (LRUCacheS*)lruCache;
+    if (NULL == cache) return 0;
+    if (cache->hashMap)
+    {
+        free(cache->hashMap);
+    }
+
+    freeList(cache);
+    free(cache);
+    return 0;
+}
+
+// 双向链表接口实现
+// 删除指定节点
+static void removeFromList(LRUCacheS *cache, cacheEntryS *entry)
+{
+    // 链表为空
+    if (cache->lruListSize == 0)
+        return;
+    
+    if (entry==cache->lruListHead && entry == cache->lruListTail)
+    {
+        // 唯一节点
+        P(&cache->cache_lock);
+        cache->lruListHead = entry->lruListNext;
+        cache->lruListHead->lruListPrev = NULL;
+        V(&cache->cache_lock);
+    }
 }
---
+++ src/ontheroad/LRU/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "lru_cache.h"
+#include "lru_cache_impl.h"
+
+int main(int argc, char **argv)
+{
+    void *LruCache;
+
+    // 创建缓存器
+    printf("hello world");
+
+    return 0;
+}
