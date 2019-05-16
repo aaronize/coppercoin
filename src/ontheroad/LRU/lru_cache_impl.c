40b624ef5d8c0c6cae804511382508a1ac585d6f
aaron.chen <aaron.chen@ucloud.cn>
2019-05-16 20:08:04 +0800

    update

--- src/ontheroad/LRU/lru_cache_impl.c
+++ src/ontheroad/LRU/lru_cache_impl.c
@@ -30,3 +30,44 @@ void V(sem_t *sem)
     if (sem_post(sem) < 0)
         unix_error
 }
+
+static void freeList(LRUCacheS *cache);
+
+// 创建一个缓存单元
+static cacheEntryS* newCacheEntry(char *key, char *data)
+{
+    cacheEntryS* entry = NULL;
+    if (NULL == (entry = malloc(sizeof(*entry)))) 
+    {
+        perror("malloc");
+        return NULL;
+    }
+    memset(entry, 0, sizeof(*entry));
+    strncpy(entry->key, key, KEY_SIZE);
+    strncpy(entry->data, data, VALUE_SIZE);
+    Sem_init(&(entry->entry_lock), 0, 1);
+
+    return entry;
+}
+
+// 释放一个缓冲单元
+static void freeCacheEntry(cacheEntryS* entry)
+{
+    if (NULL == entry) return;
+    free(entry);
+}
+
+// 创建一个LRU缓存
+int LRUCacheCreate(int capacity, void **lruCache)
+{
+    LRUCacheS* cache = NULL;
+    if (NULL == (cache = malloc(sizeof(*cache))))
+    {
+        perror("molloc");
+        return -1;
+    }
+    memset(cache, 0, sizeof(*cache));
+    cache->cacheCapacity = capacity;
+    Sem_init(&(cache->cache_lock), 0, 1);
+    cache->hashMap = malloc(sizeof(cacheEntryS*)*capacity);
+}
