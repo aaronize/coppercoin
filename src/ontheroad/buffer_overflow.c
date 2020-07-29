2ee5a30423b75ae021ce495d88f839f11b1fcc0a
aaronize <chendai2004@hotmail.com>
2020-07-30 07:10:13 +0800

    update

---
+++ src/ontheroad/buffer_overflow.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int function(int a, int b, int c)
+{
+    char buffer[14];
+    int sum;
+    int *ret;
+
+    ret = buffer + 20;
+    (*ret) += 10;
+    sum = a + b + c;
+
+    return sum;
+}
+
+int main() 
+{
+    int x; 
+
+    x = 0;
+    function(1, 2, 3);
+    x = 1;
+    printf("%d\\n", x);
+
+    return 0;
+}
